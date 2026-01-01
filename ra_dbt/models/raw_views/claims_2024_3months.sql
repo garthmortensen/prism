dc3fc15cfe018d5ff44bb915969d550d025cfd1c
Garth Mortensen <mortensengarth@hotmail.com>
2026-01-01 14:54:11 -0600

    fix(field): correct paid_date field name

--- ra_dbt/models/raw_views/claims_2024_3months.sql
+++ ra_dbt/models/raw_views/claims_2024_3months.sql
@@ -1,4 +1,4 @@
 select *
 from {{ ref('raw_claims') }}
 where year(cast(service_date as date)) = 2024
-  and cast(paid_date as date) <= '2024-03-31'
+  and cast(clean_claim_out as date) <= '2024-03-31'
--- ra_dbt/models/raw_views/claims_2024_6months.sql
+++ ra_dbt/models/raw_views/claims_2024_6months.sql
@@ -1,4 +1,4 @@
 select *
 from {{ ref('raw_claims') }}
 where year(cast(service_date as date)) = 2024
-  and cast(paid_date as date) <= '2024-06-30'
+  and cast(clean_claim_out as date) <= '2024-06-30'
